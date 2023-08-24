10fc1a174ea3f1b264d8a07d15ef7c0fb844bd54
= <=>
2023-08-24 14:50:53 +0530

    adding loading plaese wait  message

--- frontend/src/components/UI/loading/Loading.jsx
+++ frontend/src/components/UI/loading/Loading.jsx
@@ -2,9 +2,17 @@ import './loading.css';
 
 function Loading() {
   return (
-    <div className="fullscreen-loading">
-      <p className="loading">laoding...</p>
-    </div>
+    <>
+      <div className="fullscreen-loading">
+        <p className="loading">laoding...</p>
+      </div>
+      <div className="fullscreen-msg">
+        <p className="loading-msg">
+          This is an education purpose-only site hosted using free services.
+          Please wait until it loads :)
+        </p>
+      </div>
+    </>
   );
 }
 
